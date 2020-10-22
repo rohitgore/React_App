f9d4fcc8a6083a4d320853d2789112817f079efa
rohitgore <rrohit.gore@gmail.com>
2020-10-22 12:37:47 +0530

    icon issue set

--- src/app_component/GetWeatherData.component.tsx
+++ src/app_component/GetWeatherData.component.tsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 import InputForm from './InputForm.component';
 import Weather from './Weather.component';
 const API_Key = "83d9420e68892320ae0f1b0b567d9c56";
- 
-const GetWeatherData = (props:any) => {
+
+const GetWeatherData = (props: any) => {
     const [city, setcity] = useState('');
     const [country, setcountry] = useState('');
     const [weatherDescription, setweatherDescription] = useState('');
@@ -14,7 +14,7 @@ const GetWeatherData = (props:any) => {
     const [celcious, setcelcious] = useState(0);
     const [error, seterror] = useState('');
     const [image, setimage] = useState('')
- 
+
     const weathericon = {
         Thunderstorm: "poo-storm",
         Drizzle: "CloudShowersHeavy",
@@ -24,22 +24,22 @@ const GetWeatherData = (props:any) => {
         Clear: "sun",
         Cloud: "cloud"
     }
- 
+
     const weatherimage = {
         rain: "http://img.talkandroid.com/uploads/2014/06/Android-rain.jpg"
     }
- 
+
     //Calculate celcious Temprature
     const celcioustemp = (current_temp: any) => {
         let cell = Math.floor(current_temp - 273.15);
         return cell;
     }
- 
+
     //useEffect call get data after component render
     useEffect(() => {
-        getDefaultData();    
+        getDefaultData();
     }, [])
- 
+
     //Get Weather Icon on Condition
     const getWeatherIcon = (icon: any, rangeid: any) => {
         switch (true) {
@@ -51,21 +51,21 @@ const GetWeatherData = (props:any) => {
                 seticon(icon => weathericon.Rain); break;
             case rangeid >= 600 && rangeid <= 622:
                 seticon(icon => weathericon.Snow); break;
-                case rangeid >= 701 && rangeid <= 781:
-                    seticon(icon => weathericon.Atmosphere); break;
-                case rangeid == 800:
-                    seticon(icon => weathericon.Clear); break;
-                case rangeid >= 801 && rangeid <= 804:
-                    seticon(icon => weathericon.Cloud); break;
-                default:
-                    seticon(icon => weathericon.Cloud);
-            }
+            case rangeid >= 701 && rangeid <= 781:
+                seticon(icon => weathericon.Atmosphere); break;
+            case rangeid == 800:
+                seticon(icon => weathericon.Clear); break;
+            case rangeid >= 801 && rangeid <= 804:
+                seticon(icon => weathericon.Cloud); break;
+            default:
+                seticon(icon => weathericon.Cloud);
         }
-         //Get Default weather data
+    }
+    //Get Default weather data
 
-         const getDefaultData = () =>
+    const getDefaultData = () =>
     {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Pune,IN&appid=${API_Key}`)
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Mumbai,IN&appid=${API_Key}`)
         .then(res=>{setcity(city => `${res.data.name},${res.data.sys.country}`);
         setcountry(country => res.data.sys.country);
         setweatherDescription(weatherDescription => res.data.weather[0].description);
@@ -77,45 +77,45 @@ const GetWeatherData = (props:any) => {
         console.log(res);})
         .catch(err=>console.log(err))
     }
-        //Get Data From Weather API
-     
-        const getweatherData = (e: any) => {
-            e.preventDefault();
-     
-            const citynm = e.target.city.value;
-            const countrynm = e.target.country.value;
-            if (citynm && countrynm) {
-     
-                axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${citynm},${countrynm}&appid=${API_Key}`).
-                    then(res => {
-                        setcity(city => `${res.data.name},${res.data.sys.country}`);
-                        setcountry(country => res.data.sys.country);
-                        setweatherDescription(weatherDescription => res.data.weather[0].description);
-                        setcelcious(celcious => celcioustemp(res.data.main.temp));
-                        settemp_min(temp_min => celcioustemp(res.data.main.temp_min));
-                        settemp_max(temp_max => celcioustemp(res.data.main.temp_max));
-                        getWeatherIcon(weathericon, res.data.weather[0].id);
-                        console.log(res);
-                        props.loaddata(res.data.weather[0].id);
-                        seterror(err => '');
-     
-                    })
-                    .catch(err => console.log(err))
-            }
-            else {
-     
-                // alert("enter the values");
-                seterror(err => 'true');
-            }
-     
+    //Get Data From Weather API
+
+    const getweatherData = (e: any) => {
+        e.preventDefault();
+
+        const citynm = e.target.city.value;
+        const countrynm = e.target.country.value;
+        if (citynm && countrynm) {
+
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${citynm},${countrynm}&appid=${API_Key}`).
+                then(res => {
+                    setcity(city => `${res.data.name},${res.data.sys.country}`);
+                    setcountry(country => res.data.sys.country);
+                    setweatherDescription(weatherDescription => res.data.weather[0].description);
+                    setcelcious(celcious => celcioustemp(res.data.main.temp));
+                    settemp_min(temp_min => celcioustemp(res.data.main.temp_min));
+                    settemp_max(temp_max => celcioustemp(res.data.main.temp_max));
+                    getWeatherIcon(weathericon, res.data.weather[0].id);
+                    console.log(res);
+                    props.loaddata(res.data.weather[0].id);
+                    seterror(err => '');
+
+                })
+                .catch(err => console.log(err))
         }
-        return (
-            <div>
-                <InputForm loadweather={getweatherData} error={error}></InputForm>
-                <Weather cityname={city} countryname={country} description={weatherDescription}
-                    maintemp={celcious} mintemp={temp_min} maxtemp={temp_max} icons={icon}></Weather>
-            </div>
-        )
+        else {
+
+            // alert("enter the values");
+            seterror(err => 'true');
+        }
+
     }
-     
-    export default GetWeatherData;
+    return (
+        <div>
+            <InputForm loadweather={getweatherData} error={error}></InputForm>
+            <Weather cityname={city} countryname={country} description={weatherDescription}
+                maintemp={celcious} mintemp={temp_min} maxtemp={temp_max} icons={icon}></Weather>
+        </div>
+    )
+}
+
+export default GetWeatherData;
